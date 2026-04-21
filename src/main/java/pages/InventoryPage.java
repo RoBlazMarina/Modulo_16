aa0d41a43f770bb0cbae766a184a5e12b65f1a77
mrb0060 <rodriguez.blazquez.marina@gmail.com>
2026-04-21 19:15:31 +0200

    Recuperación 1

--- src/main/java/pages/InventoryPage.java
+++ src/main/java/pages/InventoryPage.java
@@ -13,27 +13,27 @@ public class InventoryPage {
     }
 
     // Localizadores
-    private By cartBadge = By.className("shopping_cart_badge");
+    private By EncontrarCarrito = By.className("shopping_cart_badge"); //La clase de nombre no se puede cambiar pues depende del CSS de la página
 
     // Método genérico para obtener el botón "Add to cart" de un producto
-    private By addToCartButton(String productName) {
-        return By.xpath("//div[text()='" + productName + "']/ancestor::div[@class='inventory_item']//button");
+    private By BotonAnadir(String NombreProducto) {
+        return By.xpath("//div[text()='" + NombreProducto + "']/ancestor::div[@class='inventory_item']//button");
     }
 
     // Método genérico para obtener el botón "Remove" de un producto
-    private By removeButton(String productName) {
-        return By.xpath("//div[text()='" + productName + "']/ancestor::div[@class='inventory_item']//button[contains(text(),'Remove')]");
+    private By removeButton(String NombreProducto) {
+        return By.xpath("//div[text()='" + NombreProducto + "']/ancestor::div[@class='inventory_item']//button[contains(text(),'Remove')]");
     }
 
     // Añadir producto al carrito
-    public void addProductToCart(String productName) {
-        driver.findElement(addToCartButton(productName)).click();
+    public void addProductToCart(String NombreProducto) {
+        driver.findElement(BotonAnadir(NombreProducto)).click();
     }
 
     // Obtener número de productos en el carrito
     public int getCartCount() {
         try {
-            return Integer.parseInt(driver.findElement(cartBadge).getText());
+            return Integer.parseInt(driver.findElement(EncontrarCarrito).getText());
         } catch (Exception e) {
             return 0; // Si no hay badge, el carrito está vacío
         }
@@ -44,8 +44,8 @@ public class InventoryPage {
         return driver.findElements(By.xpath("//*[contains(text(),'" + text + "')]")).size() > 0;
     }
 
-    public boolean isRemoveButtonVisible(String productName) {
-        return driver.findElements(removeButton(productName)).size() > 0;
+    public boolean isRemoveButtonVisible(String NombreProducto) {
+        return driver.findElements(removeButton(NombreProducto)).size() > 0;
     }
 }
 
